1483e42ce1ea82a97d0f8e4dda50c1186c5307a3
omorakwant <omasavalsys@gmail.com>
2025-04-10 11:29:24 +0100

    minshell

---
+++ minishell.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: odahriz <odahriz@student.1337.ma>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/10 10:48:17 by odahriz           #+#    #+#             */
+/*   Updated: 2025/04/10 11:28:56 by odahriz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+
+
+
+#endif
